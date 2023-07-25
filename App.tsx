d317518618e3f2d813b84c4c0e2ff5ae3532efa4
EmilPapafrita <emilbaezsalazar@gmail.com>
2023-07-25 00:14:10 -0400

    Testing commit

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import {Text, View, StyleSheet} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>Hellod !!!!</Text>
+      <Text style={styles.paragraph}>Hello World Testing Croissant!!!!</Text>
     </View>
   );
 }
